fc32197b033938831a6ca0f76da6864ea8b1d21e
Luca <lm28073@pausd.us>
2020-03-16 19:21:51 -0700

    added more parameters to the constructor

--- src/main/java/com/palyrobotics/Main.java
+++ src/main/java/com/palyrobotics/Main.java
@@ -11,7 +11,7 @@ import static com.palyrobotics.processing.VisionProcessing.*;
 public class Main {
 
     private static List<Sensor> mRunningSensors;
-    private static KumquatVision camera = new KumquatVision("127.0.0.1", 5802, 5803, true, "Vision");
+    private static KumquatVision camera = new KumquatVision("127.0.0.1", 5802, 5803, 5803, 5803, true, "Vision");
     private static String order1 = generateOrder(dilate(4), threshold(ColorConstants.kOrangeLowerBoundHSV, ColorConstants.kOrangeUpperBoundHSV), largest(1), "E copy,", "E test,", showContour());
     private static String order2 = generateOrder(erode(2), threshold(new Scalar(1, 2, 3), new Scalar(244, 244 ,244)), largest(1), showContour());
 
