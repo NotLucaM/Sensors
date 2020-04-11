a06a959438ab298d4659cee3e957d026df07a5dd
Luca <lm28073@pausd.us>
2020-04-10 17:21:51 -0700

    testing function

--- src/main/java/com/palyrobotics/Main.java
+++ src/main/java/com/palyrobotics/Main.java
@@ -1,30 +1,21 @@
 package com.palyrobotics;
 
-import com.palyrobotics.processing.VisionProcessing;
-import com.palyrobotics.sensors.KumquatVision;
 import com.palyrobotics.sensors.Sensor;
-import com.palyrobotics.util.ColorConstants;
-import com.palyrobotics.processing.VisionProcessing.*;
+import com.palyrobotics.sensors.TimeOfFlightSensor;
 
 import java.util.List;
 
-import static com.palyrobotics.processing.VisionProcessing.*;
 public class Main {
 
     private static List<Sensor> mRunningSensors;
-    private static KumquatVision camera = new KumquatVision("127.0.0.1", 5802, 5802, 5803, 5803, true, "Vision");
-    private static List<Order> order1 = generateOrder(new Erode(4), new Threshold(ColorConstants.kOrangeLowerBoundHSV, ColorConstants.kOrangeUpperBoundHSV), new FindContours(), new Largest(2));
-    private static List<Order> order2 = generateOrder(new Threshold(ColorConstants.kOrangeLowerBoundHSV, ColorConstants.kOrangeUpperBoundHSV),
-            new FindContours(), new Largest(1), new DrawContours(), new DrawCentroid(ColorConstants.kRedUpperBoundHSV));
 
     static {
-        camera.addActive(new VisionProcessing(order2));
-        mRunningSensors = List.of(camera);
+        mRunningSensors = List.of(new TimeOfFlightSensor("input"));
     }
 
     public static void main(String[] args) throws InterruptedException {
         mRunningSensors.forEach(Sensor::init);
-        Thread.sleep(10000);
-        camera.addActive(new VisionProcessing(order1));
+
+        ((TimeOfFlightSensor) mRunningSensors.get(0)).searchForPorts();
     }
 }
--- src/main/java/com/palyrobotics/sensors/TimeOfFlightSensor.java
+++ src/main/java/com/palyrobotics/sensors/TimeOfFlightSensor.java
@@ -1,8 +1,21 @@
 package com.palyrobotics.sensors;
 
+import com.fazecast.jSerialComm.SerialPort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.management.BufferPoolMXBean;
+import java.util.Enumeration;
+
 public class TimeOfFlightSensor implements Sensor {
 
+    static final  int TIMEOUT = 2000;
+
     private final String inputLocation;
+    //private CommPortIdentifier portIdentifier;
+    private SerialPort port;
 
     public TimeOfFlightSensor(String input)  {
         this.inputLocation = input;
@@ -26,5 +39,34 @@ public class TimeOfFlightSensor implements Sensor {
     @Override
     public void run() {
 
+    }
+
+    public void searchForPorts() {
+        SerialPort[] ports = SerialPort.getCommPorts();
+        for(var p : ports) {
+            System.out.println(p.getDescriptivePortName() + " " + p.getSystemPortName() + " " + p.getBaudRate());
+
+            p.setBaudRate(115200);
+            p.setComPortTimeouts(SerialPort.TIMEOUT_READ_BLOCKING, 1000, 1000);
+            p.openPort();
+            InputStream br = p.getInputStream();
+            while (true) {
+                try {
+                    int n1 = br.read();
+                    while (n1 != 0x59) {
+                        n1 = br.read();
+                    }
+                    n1 = br.read();
+                    if (n1 != 0x59) {
+                        continue;
+                    }
+                    int n2 = br.read();
+                    n2 = n2 + 256 * br.read();
+                    System.out.println(n2);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
