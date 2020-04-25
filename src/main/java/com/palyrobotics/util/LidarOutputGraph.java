cdb4b3a4e7023d97be230c077e06cbf7b6b62863
Luca <lm28073@pausd.us>
2020-04-25 12:31:50 -0700

    added function to transform polar coordinates into cartesian coordinates and added client to LidarOutput

--- src/main/java/com/palyrobotics/util/LidarOutputGraph.java
+++ src/main/java/com/palyrobotics/util/LidarOutputGraph.java
@@ -1,5 +1,8 @@
 package com.palyrobotics.util;
 
+import com.esotericsoftware.kryonet.Client;
+import com.esotericsoftware.kryonet.Connection;
+import com.esotericsoftware.kryonet.Listener;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -8,19 +11,23 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.util.Arrays;
+
 public class LidarOutputGraph extends Application {
 
-    private final String address;
-    private final int tcpPort;
+    private final String host = "";
+    private final int tcpPort = 5807;
+    private Client client;
 
-    public LidarOutputGraph(String address, int tcpPort) {
-        this.address = address;
-        this.tcpPort = tcpPort;
+    public static void main(String[] args) {
+        launch(args);
     }
-
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        setUpClient();
+
         primaryStage.setTitle("Lidar Output");
         Group root = new Group();
         Canvas canvas = new Canvas(500, 500);
@@ -31,9 +38,32 @@ public class LidarOutputGraph extends Application {
         primaryStage.show();
     }
 
-    public void drawShapes(GraphicsContext gc) {
+    private void setUpClient() throws IOException {
+        Client client = new Client();
+        client.getKryo().register(float[].class);
+        client.addListener(new Listener() {
+            @Override
+            public void connected(Connection connection) {
+                System.out.println("Connected");
+            }
+
+            @Override
+            public void received(Connection connection, Object object) {
+                System.out.println(Arrays.toString((float[]) object));
+            }
+        });
+        new Thread(client).start();
+
+        client.connect(4000, host, tcpPort);
+    }
+
+    private void drawShapes(GraphicsContext gc) {
         gc.setFill(Color.RED);
         gc.setStroke(Color.BLACK);
         gc.fillRoundRect(10, 110, 30, 30, 20, 25);
+    }
+
+    public float[] getCartesian(float[] polar) {
+        return new float[]{(float) (polar[1] * Math.cos(polar[0])), (float) (polar[1] * Math.sin(polar[0]))};
     }
 }
