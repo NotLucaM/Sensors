f34988b4575d256066f463c17e6ebc6b7a55cd56
Luca <lm28073@pausd.us>
2020-04-29 21:06:30 -0700

    added a function to transform a point

--- src/main/java/com/palyrobotics/util/Point.java
+++ src/main/java/com/palyrobotics/util/Point.java
@@ -2,9 +2,9 @@ package com.palyrobotics.util;
 
 public class Point {
 
-    int x, y;
+    double x, y;
 
-    public Point(int x, int y) {
+    public Point(double x, double y) {
         this.x = x;
         this.y = y;
     }
--- src/main/java/com/palyrobotics/util/Transform.java
+++ src/main/java/com/palyrobotics/util/Transform.java
@@ -1,4 +1,27 @@
 package com.palyrobotics.util;
 
 public class Transform {
+
+    public final double theta, tx, ty;
+
+    public Transform() {
+        theta = 0;
+        tx = ty = 0;
+    }
+
+    // Theta is in radians obviously cus im not savage also cus im lazy and dont want to convert to radians each time i use math
+    public Transform(double theta, double tx, double ty) {
+        this.theta = theta;
+        this.tx = tx;
+        this.ty = ty;
+    }
+
+    // The following functions apply the transform onto various classes
+    public Point apply(Point t) {
+        return new Point(t.x * Math.cos(theta)- t.y * Math.sin(theta),
+                        t.x * Math.sin(theta) - t.y * Math.cos(theta));
+    }
+
+//    public PointCloud apply(PointCloud t) {
+//    }
 }
