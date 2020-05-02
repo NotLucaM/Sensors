75986ca1085de386f090143a28dfb74c613963ff
Luca <lm28073@pausd.us>
2020-05-01 20:48:39 -0700

    begun work on ICP

--- src/main/java/com/palyrobotics/util/ICP.java
+++ src/main/java/com/palyrobotics/util/ICP.java
@@ -1,4 +1,11 @@
 package com.palyrobotics.util;
 
 public class ICP {
+
+    private PointCloud reference; // The field
+    private long timeout; // Timeout for each call of doICP in nano seconds
+
+    public Transform doICP(PointCloud lidarOutput) {
+        return null;
+    }
 }
