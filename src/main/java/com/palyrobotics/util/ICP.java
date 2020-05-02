01947f00780dd48ec903261b47a2b66a9114e70d
Luca <lm28073@pausd.us>
2020-05-01 21:03:39 -0700

    added anouther doICP without transform as input

--- src/main/java/com/palyrobotics/util/ICP.java
+++ src/main/java/com/palyrobotics/util/ICP.java
@@ -6,6 +6,15 @@ public class ICP {
     private long timeout; // Timeout for each call of doICP in nano seconds
 
     public Transform doICP(PointCloud lidarOutput) {
+        return doICP(lidarOutput, new Transform());
+    }
+
+    public Transform doICP(PointCloud lidarOutput, Transform transform) {
+        long startingTime = System.nanoTime();
+
+        while (startingTime - System.nanoTime() <= timeout) {
+
+        }
         return null;
     }
 }
